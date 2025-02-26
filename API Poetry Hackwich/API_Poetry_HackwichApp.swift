8c6571da4424a885a6be742cededbce6716d5971
dmyers1048 <dmyers1@bsd220.org>
2025-02-26 09:41:17 -0600

    Initial Commit

---
+++ API Poetry Hackwich/API_Poetry_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Poetry_HackwichApp.swift
+//  API Poetry Hackwich
+//
+//  Created by Devan Myers on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct API_Poetry_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
