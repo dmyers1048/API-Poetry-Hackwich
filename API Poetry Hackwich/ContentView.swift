d311a662d35d0c9a56ef63233d457495bec3f895
dmyers1048 <dmyers1@bsd220.org>
2025-02-27 09:36:16 -0600

    Finished setting up alert

--- API Poetry Hackwich/ContentView.swift
+++ API Poetry Hackwich/ContentView.swift
@@ -13,6 +13,7 @@ struct Authors: Codable {
 
 struct ContentView: View {
     @State private var authors = [String]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(authors, id:\.self) { author in
@@ -26,6 +27,11 @@ struct ContentView: View {
         .task {
             await getAuthors()
         }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text ("Loading Error"),
+                  message: Text("There was a problem loading the poetry authors"),
+                  dismissButton: .default(Text("OK")))
+        }
     }
     func getAuthors() async {
             let query = "https://poetrydb.org//author"
@@ -33,9 +39,11 @@ struct ContentView: View {
                 if let (data, _) = try? await URLSession.shared.data(from: url) {
                     if let decodedResponse = try? JSONDecoder().decode(Authors.self, from: data) {
                         authors = decodedResponse.authors
+                        return
                     }
                 }
             }
+            showingAlert = true
         }
 }
 
